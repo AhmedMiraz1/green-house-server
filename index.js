4da63f53dea822ec389b6f38d1f6bd08249b05b6
mmirazz <mehedimiraz900@gmail.com>
2024-06-20 00:26:49 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -33,6 +34,105 @@ async function run() {
     await client.connect();
 
     const apartmentCollection = client.db("greenHouse").collection("apartment");
+    const agreementCollection = client.db("greenHouse").collection("agreement");
+    const userCollection = client.db("greenHouse").collection("users");
+
+   // jwt
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
+
+    //middleware
+
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token ', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    // use verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    //user related api
+
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: " forbidden access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      return res.send({ admin });
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert email if user doesn't exists :
+      //you can do this many way (1, email unique , 2, upsert , 3. simple  checking   )
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist ", insertId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // apartment api 
 
@@ -40,6 +140,34 @@ async function run() {
         const result = await apartmentCollection.find().toArray();
         res.send(result);
       });
+
+
+      // card collection api 
+
+      app.post("/agreement", async (req, res) => {
+        const cartItem = req.body;
+        const result = await agreementCollection.insertOne(cartItem);
+        res.send(result);
+      });
+
+
+      // pagination 
+
+      app.get("/all-apartments", async (req, res) => {
+        const size =parseInt(req.query.size)
+        const page =parseInt(req.query.page) -1
+        console.log(size, page);
+        const result = await apartmentCollection.find().skip(page * size).limit(size).toArray();
+        res.send(result);
+      });
+
+      app.get("/apartments-count", async (req, res) => {
+        const count = await apartmentCollection.countDocuments()
+        res.send({count});
+      });
+
+      
+
 
 
 
