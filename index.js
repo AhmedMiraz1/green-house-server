f7c40710c38655e6b3ea1e8904a762bcfad5acaf
mmirazz <mehedimiraz900@gmail.com>
2024-06-25 16:30:55 +0600

    jwt token refresh

--- index.js
+++ index.js
@@ -49,13 +49,11 @@ async function run() {
 
     // jwt
 
-    app.post("/jwt", async (req, res) => {
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "7d",
-      });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res.send({ token });
-    });
+    })
 
     //middleware
 
